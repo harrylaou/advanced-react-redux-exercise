5650519d7bbaa5324012def00368ae017a0d96b8
alexlbr <alex.lobera@gmail.com>
2017-02-10 15:56:43 +0000

    solution midd chain

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,38 +1,45 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 
-const addLoggerMiddleware = store => {
-  const rawDispatch = store.dispatch
+const addLoggerMiddleware = store => next => {
   return (action) => {
     console.group(action.type);
     console.log('prev state', store.getState());
     console.log('action', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = next(action);
     console.log('next state', store.getState());
     console.groupEnd(action.type);
     return returnValue;
   }
 }
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+const addPromiseSupportToDispatch = (store) => next => {
   return (action) => {
     if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
+      return action.then(next);
     }
-    return rawDispatch(action);
-  };
-};
+    return next(action);
+  }
+}
 
 const configureStore = () => {
   const store = createStore(
     reducers
   )
-
-  store.dispatch = addLoggerMiddleware(store)
-  store.dispatch = addPromiseSupportToDispatch(store)
+/*
+  store.dispatch = addLoggerMiddleware(store)(store.dispatch)
+  store.dispatch = addPromiseSupportToDispatch(store)(store.dispatch)
+*/
+  wrapDispatchWithMiddlewares(store, [addPromiseSupportToDispatch, addLoggerMiddleware])
 
   return store
 }
+
+const wrapDispatchWithMiddlewares = (store, middlewares) =>
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch)
+  })
+
+// wrapDispatchWithMiddlewares(store, [addPromiseSupportToDispatch, addLoggerMiddleware])
 
 export default configureStore
