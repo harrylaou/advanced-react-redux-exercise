314c868970768d6883c42f8d1c411498f723c32c
alexlbr <alex.lobera@gmail.com>
2017-02-10 16:10:04 +0000

    added solution_redux_middleware

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,40 +1,18 @@
+import createLogger from 'redux-logger'
+import promiseMiddleware from 'redux-promise'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 
-const addLoggerMiddleware = (store) => (next) => {
-  return (action) => {
-    console.group(action.type);
-    console.log('prev state', store.getState());
-    console.log('action', action);
-    const returnValue = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  }
-}
-
-const addPromiseSupportToDispatch = (store) => (next) => {
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(next);
-    }
-    return next(action);
-  }
-}
-
 const configureStore = () => {
   const store = createStore(
-    reducers
+    reducers,
+    applyMiddleware(
+      promiseMiddleware,
+      createLogger()
+    )
   )
-
-  wrapDispatchWithMiddlewares(store, [ addPromiseSupportToDispatch, addLoggerMiddleware ])
 
   return store
 }
-
-const wrapDispatchWithMiddlewares = (store, middlewares) =>
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
 
 export default configureStore
