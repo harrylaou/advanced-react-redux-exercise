98957937b60ff4281209cda17a29589e134fea78
alexlbr <alex.lobera@gmail.com>
2017-02-10 15:31:39 +0000

    added logger midd

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,10 +1,25 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 
+const addLoggerMiddleware = store => {
+  const rawDispatch = store.dispatch
+  return (action) => {
+    console.group(action.type);
+    console.log('prev state', store.getState());
+    console.log('action', action);
+    const returnValue = rawDispatch(action);
+    console.log('next state', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  }
+}
+
 const configureStore = () => {
   const store = createStore(
     reducers
   )
+
+  store.dispatch = addLoggerMiddleware(store)
 
   return store
 }
