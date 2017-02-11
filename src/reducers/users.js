e1edadac45fe68e5db9e51cd7d8320304a20aeb4
Marc Rivelles <rivelles.marc@gmail.com>
2017-02-11 18:32:04 +0000

    Update users.js

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,13 +2,12 @@ import { RECEIVE_USERS, FETCHING_USERS } from '../actions/users'
 
 const users = (state = { items: [], nextUrl: null }, action) => {
   switch (action.type) {
-    case RECEIVE_USERS: {
+    case RECEIVE_USERS:
       return {
         ...state,
         items: [...state.items, ...action.users.items],
         nextUrl: action.nextUrl
       }
-    }
     default:
       return state
   }
