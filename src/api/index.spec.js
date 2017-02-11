a35cef85d8bfc762fa1f889e3e039a6c8af0d185
Marc Rivelles <rivelles.marc@gmail.com>
2017-02-11 18:56:36 +0000

    Updated

--- src/api/index.spec.js
+++ src/api/index.spec.js
@@ -1,5 +1,4 @@
 import { expect } from 'chai'
-import {setMessageTo, SET_MESSAGE_TO} from './message'
 
 describe('#API fetchUsers:', ()=>{
   it('Should return object with users and nextUrl', ()=>{
